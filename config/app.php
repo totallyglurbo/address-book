ecf5753f62c4c9979a4ed724c38d252ade6c3150
totallyglurbo <glurb@glurbin.com>
2026-05-26 13:47:26 +0700

    Add basic structure Added several files for requests

---
+++ config/app.php
@@ -0,0 +1,18 @@
+<?php
+return [
+    'auth' => \Src\Auth\Auth::class,
+    'identity'=>\Model\User::class,
+    'routeMiddleware' => [
+        'auth' => \Middlewares\AuthMiddleware::class,
+    ],
+    'routeAppMiddleware' => [
+        'csrf' => \Middlewares\CSRFMiddleware::class,
+        'trim' => \Middlewares\TrimMiddleware::class,
+        'specialChars' => \Middlewares\SpecialCharsMiddleware::class,
+    ],
+    'validators' => [
+        'required' => \Validators\RequireValidator::class,
+        'unique' => \Validators\UniqueValidator::class,
+        'lang' => \Validators\LangValidator::class,
+    ]
+];
---
+++ config/db.php
@@ -0,0 +1,11 @@
+<?php
+return [
+    'driver' => 'mysql',
+    'host' => 'localhost',
+    'database' => 'hxvcdtjv_m1',
+    'username' => 'root',
+    'password' => 'sF8p2Y',
+    'charset' => 'utf8',
+    'collation' => 'utf8_unicode_ci',
+    'prefix' => '',
+];
